83826e241bfc25eea170a56f6a949c0630c04df0
Christopher Rush <ceo@christopherhustle.com>
2022-08-11 14:31:48 +0200

    ADD method for getting log entries with date filter and pagination

--- src/main/java/com/hastesoft/logparser/controller/LogEntryController.java
+++ src/main/java/com/hastesoft/logparser/controller/LogEntryController.java
@@ -3,11 +3,16 @@ package com.hastesoft.logparser.controller;
 import com.hastesoft.logparser.model.LogEntry;
 import com.hastesoft.logparser.service.LogEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -28,5 +33,20 @@ public class LogEntryController {
     public ResponseEntity<List<LogEntry>> getAll() {
         List<LogEntry> result = logEntryService.getAll();
         return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/search", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Page<LogEntry>> getLogEntriesWithDateFilterAndPagination(
+            @RequestParam(defaultValue = "") String dateFrom,
+            @RequestParam(defaultValue = "") String dateTo,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "") List<String> sortList,
+            @RequestParam(defaultValue = "DESC") Sort.Direction sortDirection
+            ) {
+        LocalDateTime dateFromFilter = !dateFrom.isEmpty() ? LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern("dd-MM-yyyy")).atStartOfDay() : null;
+        LocalDateTime dateToFilter = !dateTo.isEmpty() ? LocalDate.parse(dateTo, DateTimeFormatter.ofPattern("dd-MM-yyyy")).atStartOfDay() : null;
+        Page<LogEntry> logEntryPage = logEntryService.getLogEntriesAsPageWithDateFilteringAndSorting(dateFromFilter, dateToFilter, page, size, sortList, sortDirection.toString());
+        return new ResponseEntity<>(logEntryPage, HttpStatus.OK);
     }
 }
--- src/main/java/com/hastesoft/logparser/service/LogEntryService.java
+++ src/main/java/com/hastesoft/logparser/service/LogEntryService.java
@@ -3,10 +3,15 @@ package com.hastesoft.logparser.service;
 import com.hastesoft.logparser.model.LogEntry;
 import com.hastesoft.logparser.repository.LogEntryRepository;
 import com.hastesoft.logparser.util.LogParser;
-import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,5 +26,44 @@ public class LogEntryService {
 
     public List<LogEntry> getAll() {
         return logEntryRepository.findAll();
+    }
+
+    public Page<LogEntry> getLogEntriesAsPageWithDateFilteringAndSorting(
+            LocalDateTime dateFromFilter,
+            LocalDateTime dateToFilter,
+            int page,
+            int size,
+            List<String> sortList,
+            String sortOrder
+    ) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(createSortOrder(sortList, sortOrder)));
+        Page<LogEntry> result = null;
+        if (dateFromFilter != null && dateToFilter != null) {
+            result = logEntryRepository.findByDateFromAndDateToFilter(dateFromFilter, dateToFilter, pageable);
+        }
+        if (dateFromFilter == null) {
+            result = logEntryRepository.findByDateToFilter(dateToFilter, pageable);
+        }
+        if (dateToFilter == null) {
+            result = logEntryRepository.findByDateFromFilter(dateFromFilter, pageable);
+        }
+        if (dateFromFilter == null && dateToFilter == null) {
+            result = logEntryRepository.findAllPageable(pageable);
+        }
+        return result;
+    }
+
+    private List<Sort.Order> createSortOrder(List<String> sortList, String sortDirection) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        Sort.Direction direction;
+        for (String sort : sortList) {
+            if (sortDirection != null) {
+                direction = Sort.Direction.fromString(sortDirection);
+            } else {
+                direction = Sort.Direction.DESC;
+            }
+            sorts.add(new Sort.Order(direction, sort));
+        }
+        return sorts;
     }
 }
